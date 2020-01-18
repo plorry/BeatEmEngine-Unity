9052e8453da8cf5295f81e1b6134db08188993ba
plorry <timebot@gmail.com>
2020-01-18 14:52:32 -0800

    target for AI

--- Assets/AIBattlerBehaviour.cs
+++ Assets/AIBattlerBehaviour.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class AIBattlerBehaviour : BattlerBehaviour
 {
-
+    private BattlerBehaviour target; // the target Battler
+    private Transform targetSpot; // the target Spot to follow
 }
